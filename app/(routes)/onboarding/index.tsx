d693d0d71c266f053e1517b6b639a3e09a0aa2f8
Qiannnly <qianlinggg@gmail.com>
2024-11-09 00:29:00 +0800

    added onboarding and authentication routes

---
+++ app/(routes)/onboarding/index.tsx
@@ -0,0 +1,11 @@
+import OnboardingScreen from "@/screens/OnboardingScreen";
+
+const Onboarding = () => {
+  return (
+    <>
+      <OnboardingScreen />
+    </>
+  );
+};
+
+export default Onboarding;
---
+++ app/(routes)/register/index.tsx
@@ -0,0 +1,7 @@
+import SignupScreen from "@/screens/SignupScreen";
+
+const Register = () => {
+  return <SignupScreen />;
+};
+
+export default Register;
