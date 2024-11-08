7034701e48b5af1be30e5eea842a5a60a7aa0b7d
Qiannnly <qianlinggg@gmail.com>
2024-11-09 00:15:34 +0800

    added reusable button components

--- components/button/Buttons.tsx
+++ components/button/Buttons.tsx
@@ -1,17 +1,36 @@
 import { ReactNode } from "react";
-import { GestureResponderEvent } from "react-native";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+  GestureResponderEvent,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 type ButtonProps = {
   children?: ReactNode;
   onPress: (event: GestureResponderEvent) => void;
 };
+
+type LogoutButtonProps = {
+  onPress: (event: GestureResponderEvent) => void;
+};
+
 export const Button = ({ children, onPress }: ButtonProps) => {
   return (
     <>
       <TouchableOpacity style={styles.button} onPress={onPress}>
         <Text style={styles.text}>{children}</Text>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+export const LogoutButton = ({ onPress }: LogoutButtonProps) => {
+  return (
+    <>
+      <TouchableOpacity style={styles.logoutButton} onPress={onPress}>
+        <AntDesign name="logout" size={30} color="black" />
       </TouchableOpacity>
     </>
   );
@@ -31,5 +50,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "white",
     fontFamily: "poppins-medium",
+  },
+  logoutButton: {
+    position: "absolute",
+    right: 60,
+    top: 100,
   },
 });
