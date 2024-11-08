22492e2b058ea996460430200736415b0a3b0e31
Qiannnly <qianlinggg@gmail.com>
2024-11-09 00:30:43 +0800

    added route to create destination

---
+++ app/destination/index.tsx
@@ -0,0 +1,11 @@
+import CreateDestinationScreen from "@/screens/CreateScreen/destination";
+
+const Destination = () => {
+  return (
+    <>
+      <CreateDestinationScreen />
+    </>
+  );
+};
+
+export default Destination;
