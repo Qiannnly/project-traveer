e9f0bca68fc15d67dbc816f21adc406f113fb4ed
Qiannnly <qianlinggg@gmail.com>
2024-11-10 22:51:28 +0800

    added function to reload trips after deleting a trip

--- components/trip/Trip.tsx
+++ components/trip/Trip.tsx
@@ -1,18 +1,61 @@
-import { Image, View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  Image,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
 import { router } from "expo-router";
 import { AntDesign } from "@expo/vector-icons";
 import { TripListTypes } from "@/types/global";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import axios from "axios";
 
-const Trip = ({ trip }: { trip: TripListTypes }) => {
+const Trip = ({
+  trip,
+  reloadTrips,
+}: {
+  trip: TripListTypes;
+  reloadTrips: () => void;
+}) => {
   const tripId = trip.id;
 
   const tripTitle = trip.name;
 
   const startDate = trip.startDate.slice(0, 10);
   const endDate = trip.endDate.slice(0, 10);
+
+  const handleDelete = async (id: string) => {
+    try {
+      await axios.delete(`${process.env.EXPO_PUBLIC_BACKEND_URL}/trips/${id}`);
+      reloadTrips();
+      router.push({
+        pathname: "/(tabs)/home",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleAlert = (id: string) =>
+    Alert.alert("Remove Trip", "Are you sure you want to remove trip?", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      {
+        text: "Remove",
+        onPress: () => handleDelete(id),
+      },
+    ]);
+
   return (
     <TouchableOpacity
       style={styles.tripsContainer}
+      accessibilityLabel="Delete Trip"
+      accessibilityRole="button"
       onPress={() =>
         router.push({
           pathname: "/destination",
@@ -21,6 +64,12 @@ const Trip = ({ trip }: { trip: TripListTypes }) => {
       }
     >
       <Image source={{ uri: trip.photoUrl }} style={styles.image} />
+      <TouchableOpacity
+        onPress={() => handleAlert(tripId)}
+        style={styles.ellipsis}
+      >
+        <Ionicons name="ellipsis-horizontal-sharp" size={24} color="black" />
+      </TouchableOpacity>
       <View style={styles.tripDetails}>
         <Text style={[styles.text, styles.tripText]} numberOfLines={3}>
           {trip.name}
@@ -43,6 +92,15 @@ export default Trip;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  ellipsis: {
+    position: "absolute",
+
+    alignSelf: "flex-end",
+    right: 30,
+
+    top: 10,
   },
 
   tripContainerWithoutTrips: {
